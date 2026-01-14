eb7124ed1e0fe1dfc23c19a4154d1683b208fffa
Martin-Louis Bright <mlbright@gmail.com>
2026-01-13 22:11:57 -0500

    add account type

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_14_022340) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_14_030752) do
   create_table "accounts", force: :cascade do |t|
+    t.integer "account_type", default: 0, null: false
     t.datetime "archived_at"
     t.string "color"
     t.datetime "created_at", null: false
